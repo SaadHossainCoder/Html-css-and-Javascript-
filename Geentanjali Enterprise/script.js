d8e2bc6e4d50045da612b2245cca71b51f2a19e0
MD.Saad Hossain <hossainsaad324@gmail.com>
2024-12-08 01:19:04 +0530

    Add files via upload

---
+++ Geentanjali Enterprise/script.js
@@ -0,0 +1,12 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+});
+
+// gsap.to(".team__info", {
+//     transform: "translateX(-100%)",
+//     delay:1,
+//     duration: 10,
+//     repeat:-1,
+//     ease: "none",
+// });
